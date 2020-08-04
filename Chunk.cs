2fde96e4c3d08eabce6e83e37be3a4e2b69a6840
mrrobloxtutorialsgo <69199455+mrrobloxtutorialsgo@users.noreply.github.com>
2020-08-04 13:52:56 +0200

    Add files via upload

---
+++ Chunk.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk
+{
+	public GameObject chunkObject;
+	MeshFilter meshFilter;
+	MeshCollider meshCollider;
+	MeshRenderer meshRenderer;
+
+	Vector3Int chunkPosition;
+
+	TerrainPoint[,,] terrainMap;
+
+	List<Vector3> vertices = new List<Vector3>();
+	List<int> triangles = new List<int>();
+	List<Color> colors = new List<Color>();
+
+	int width { get { return GameData.ChunkWidth; } }
+	int height { get { return GameData.ChunkHeight; } }
+	float terrainSurface { get { return GameData.terrainSurface; } }
+
+	public Chunk(Vector3Int _position)
+	{
+		chunkObject = new GameObject();
+		chunkObject.name = string.Format("Chunk {0}, {1}", _position.x, _position.z);
+		chunkPosition = _position;
+		chunkObject.transform.position = chunkPosition;
+
+		meshFilter = chunkObject.AddComponent<MeshFilter>();
+		meshCollider = chunkObject.AddComponent<MeshCollider>();
+		meshRenderer = chunkObject.AddComponent<MeshRenderer>();
+		meshRenderer.material = Resources.Load<Material>("Materials/Terrain");
+		chunkObject.transform.tag = "Terrain";
+		terrainMap = new TerrainPoint[width + 1, height + 1, width + 1];
+		PopulateTerrainMap();
+		CreateMeshData();
+	}
+
+	void CreateMeshData()
+	{
+
+		ClearMeshData();
+
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				for (int z = 0; z < width; z++)
+				{
+					MarchCube(new Vector3Int(x, y, z));
+
+				}
+			}
+		}
+
+		BuildMesh();
+
+	}
+
+	void PopulateTerrainMap()
+	{
+		for (int x = 0; x < width + 1; x++)
+		{
+			for (int z = 0; z < width + 1; z++)
+			{
+				for (int y = 0; y < height + 1; y++)
+				{
+					float thisHeight;
+
+					thisHeight = GameData.GetTerrainHeight(x + chunkPosition.x, z + chunkPosition.z);
+
+					terrainMap[x, y, z] = new TerrainPoint((float)y - thisHeight, Random.Range(0, 2));
+
+				}
+			}
+		}
+	}
+
+	void MarchCube(Vector3Int position)
+	{
+		float[] cube = new float[8];
+		for (int i = 0; i < 8; i++)
+		{
+			cube[i] = SampleTerrain(position + GameData.CornerTable[i]);
+		}
+
+		int configIndex = GetCubeConfiguration(cube);
+
+		if (configIndex == 0 || configIndex == 255)
+			return;
+
+		int edgeIndex = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int p = 0; p < 3; p++)
+			{
+
+				int indice = GameData.TriangleTable[configIndex, edgeIndex];
+
+				if (indice == -1)
+					return;
+
+				Vector3 vert1 = position + GameData.CornerTable[GameData.EdgeIndexes[indice, 0]];
+				Vector3 vert2 = position + GameData.CornerTable[GameData.EdgeIndexes[indice, 1]];
+
+				Vector3 vertPosition;
+
+				float vert1Sample = cube[GameData.EdgeIndexes[indice, 0]];
+				float vert2Sample = cube[GameData.EdgeIndexes[indice, 1]];
+
+				float difference = vert2Sample - vert1Sample;
+
+				if (difference == 0)
+					difference = terrainSurface;
+				else
+					difference = (terrainSurface - vert1Sample) / difference;
+
+				vertPosition = vert1 + ((vert2 - vert1) * difference);
+				triangles.Add(VertForIndice(vertPosition, position));
+				edgeIndex++;
+
+			}
+		}
+	}
+
+	int GetCubeConfiguration(float[] cube)
+	{
+		int configurationIndex = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			if (cube[i] > terrainSurface)
+				configurationIndex |= 1 << i;
+
+		}
+
+		return configurationIndex;
+
+	}
+
+	public void PlaceTerrain(Vector3 pos)
+	{
+
+		Vector3Int v3Int = new Vector3Int(Mathf.CeilToInt(pos.x), Mathf.CeilToInt(pos.y), Mathf.CeilToInt(pos.z));
+		v3Int -= chunkPosition;
+		terrainMap[v3Int.x, v3Int.y, v3Int.z].dstToSurface = 0f;
+		CreateMeshData();
+
+	}
+
+	public void RemoveTerrain(Vector3 pos)
+	{
+
+		Vector3Int v3Int = new Vector3Int(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y), Mathf.FloorToInt(pos.z));
+		v3Int -= chunkPosition;
+		terrainMap[v3Int.x, v3Int.y, v3Int.z].dstToSurface = 1f;
+		CreateMeshData();
+
+	}
+
+	float SampleTerrain(Vector3Int point)
+	{
+		return terrainMap[point.x, point.y, point.z].dstToSurface;
+	}
+
+	int VertForIndice(Vector3 vert, Vector3Int point)
+	{
+		for (int i = 0; i < vertices.Count; i++)
+		{
+			if (vertices[i] == vert)
+				return i;
+		}
+
+		vertices.Add(vert);
+		colors.Add((terrainMap[point.x, point.y, point.z].textureID == 1) ? Color.black : Color.red);
+		return vertices.Count - 1;
+	}
+
+	void ClearMeshData()
+	{
+		vertices.Clear();
+		triangles.Clear();
+		colors.Clear();
+	}
+
+	void BuildMesh()
+	{
+
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.colors = colors.ToArray();
+		mesh.RecalculateNormals();
+		meshFilter.mesh = mesh;
+		meshCollider.sharedMesh = mesh;
+	}
+}
---
+++ World.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class World : MonoBehaviour
+{
+    private static World _instance;
+    public static World Instance { get { return _instance; } }
+
+    public Texture2D[] terrainTextures;
+    [HideInInspector]
+    public Texture2DArray terrainTexArray;
+
+    public int dayStartTime = 240;
+    public int dayEndTime = 1360;
+    private int dayLength { get { return dayEndTime - dayStartTime; } }
+    private float sunDayRotationPerMinute { get { return 180f / dayLength; }  }
+    private float sunNightRotationPerMinute { get { return 180f / (1440 - dayLength); } }
+
+    public Transform sun;
+    public TextMeshProUGUI clock;
+
+    public int HordeNightFrequency = 7;
+
+    public bool isHordeNight
+    {
+        get {
+            if (Day % HordeNightFrequency == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    [Range(4f, 0f)]
+    public float ClockSpeed = 1f;
+
+    public int Day = 1;
+
+    [SerializeField] private int _timeOfDay;
+    public int TimeOfDay
+    {
+        get { return _timeOfDay; }
+        set
+        {
+            _timeOfDay = value;
+
+            if (_timeOfDay > 1439) {
+                _timeOfDay = 0;
+                Day++;
+            }
+
+            UpdateClock();
+
+            float rotAmount;
+
+            if (_timeOfDay > dayStartTime && _timeOfDay < dayEndTime)
+            {
+                rotAmount = (_timeOfDay - dayStartTime) * sunDayRotationPerMinute;
+            } else if (_timeOfDay >= dayEndTime)
+            {
+                rotAmount = dayLength * sunDayRotationPerMinute;
+
+                rotAmount += ((_timeOfDay - dayStartTime - dayLength) * sunNightRotationPerMinute);
+            } else
+            {
+                rotAmount = dayLength * sunDayRotationPerMinute;
+                rotAmount += (1440 - dayEndTime) * sunNightRotationPerMinute;
+                rotAmount += _timeOfDay * sunNightRotationPerMinute;
+            }
+
+            sun.eulerAngles = new Vector3(rotAmount, 0f, 0f);
+        }
+    }
+
+    private void UpdateClock()
+    {
+        int hours = TimeOfDay / 60;
+        int minutes = TimeOfDay - (hours * 60);
+
+        string dayText;
+        if (isHordeNight)
+            dayText = string.Format("<color=red>{0}</color>", Day.ToString());
+        else
+            dayText = Day.ToString();
+
+        clock.text = string.Format("DAY: {0} TIME:  {1}:{2}", dayText, hours.ToString("D2"), minutes.ToString("D2"));
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Debug.LogWarning("More than one instance of World Present. Removing additional instance.");
+            Destroy(this.gameObject);
+        }
+        else
+            _instance = this;
+
+        PopulateTextureArray();
+    }
+
+    private float secondCounter = 0;
+
+    private void Update()
+    {
+        secondCounter += Time.deltaTime;
+        if (secondCounter > ClockSpeed)
+        {
+            TimeOfDay++;
+            secondCounter = 0;
+        }
+    }
+
+    void PopulateTextureArray()
+    {
+        terrainTexArray = new Texture2DArray(1024, 1024, terrainTextures.Length, TextureFormat.ARGB32, false);
+        Debug.Log(terrainTexArray);
+        for (int i = 0; i < terrainTextures.Length; i++)
+        {
+            terrainTexArray.SetPixels(terrainTextures[i].GetPixels(0), i, 0);
+        }
+        terrainTexArray.Apply();
+    }
+}
